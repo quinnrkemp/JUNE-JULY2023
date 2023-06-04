518d726b5eaf57752d437b1c61d06de661c9dd40
quinnrkemp <a467814@gmail.com>
2023-06-04 02:15:57 -0400

    coding chall for 060423

---
+++ 060423.js
@@ -0,0 +1,30 @@
+// CHALLENGE NAME- Multiply
+
+// This code does not execute properly. Try to figure out why.
+
+// ***THE BAD CODE***
+
+// function multiply(a, b){
+//     a * b
+//   }
+
+// ******THE TEST CASES*****
+
+// const assert = require("chai").assert;
+
+// describe("Multiply", () => {
+//   it("fixed tests", () => {
+//     assert.strictEqual(multiply(1,1), 1);
+//     assert.strictEqual(multiply(2,1), 2);
+//     assert.strictEqual(multiply(2,2), 4);
+//     assert.strictEqual(multiply(3,5), 15);   
+//   });
+// });
+  
+//   ****MY ANSWER****
+
+  function multiply(a, b){
+    return a * b
+  }
+
+
