506e1f0ac91dc7b4628f84f70b72d5c7b6ffd681
quinnrkemp <a467814@gmail.com>
2023-06-09 18:22:59 -0400

    coding chall for 060123

---
+++ 060123.js
@@ -0,0 +1,30 @@
+// CHALLENGE NAME- Jenny's secret message
+
+// Jenny has written a function that returns a greeting for a user. However, she's in love with Johnny, and would like to 
+// greet him slightly different. She added a special case to her function, but she made a mistake.
+
+// Can you help her?
+
+// ******TEST CASES*******
+
+// const Test = require('@codewars/test-compat');
+
+// describe("Jenny's greeting function", function(){
+//   it("should greet some people normally",function(){
+//     Test.assertEquals(greet("Jim"), "Hello, Jim!");
+//     Test.assertEquals(greet("Jane"), "Hello, Jane!");
+//     Test.assertEquals(greet("Simon"), "Hello, Simon!");
+//   });
+//   it("should greet Johnny a little bit more special",function(){
+//     Test.assertEquals(greet("Johnny"), "Hello, my love!");
+//   });
+// });
+
+// ******MY ANSWER*******
+
+function greet(name){
+    return "Hello, " + name + "!";
+    if(name === "Johnny")
+      return "Hello, my love!";
+  }
+
