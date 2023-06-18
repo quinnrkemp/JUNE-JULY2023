d5e5773c75b902f76e150c0110f4d520a4b02702
quinnrkemp <a467814@gmail.com>
2023-06-18 01:44:57 -0400

    coding chall for 061823

---
+++ 061823.js
@@ -0,0 +1,35 @@
+// CHALLENGE NAME-  RETURNING STRINGS
+
+// DESCRIPTION:
+
+// Make a function that will return a greeting statement that uses an input; your program should 
+// return, "Hello, <name> how are you doing today?".
+
+// [Make sure you type the exact thing I wrote or the program may not execute properly]
+
+
+// ***STARTER CODE***
+// function greet(name){
+//     //your code here
+//   }
+ 
+//   ******TEST CASES*****
+
+// const chai = require("chai");
+// const assert = chai.assert;
+// chai.config.truncateThreshold=0;
+
+// describe("Basic tests",() =>{
+//   it("Testing for fixed tests", () => {
+//     assert.strictEqual(greet("Ryan"), "Hello, Ryan how are you doing today?");
+//     assert.strictEqual(greet("Shingles"), "Hello, Shingles how are you doing today?");
+//   })
+// })
+
+ 
+// ******MY ANSWER********
+
+function greet(name){
+    //your code here
+    return `Hello, ${name} how are you doing today?`
+  }
