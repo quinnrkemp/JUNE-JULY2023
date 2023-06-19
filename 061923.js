f5da0b6757bfcdfdeacb8d3234d4f09a4b685815
quinnrkemp <a467814@gmail.com>
2023-06-19 01:53:35 -0400

    coding chall for 061923

---
+++ 061923.js
@@ -0,0 +1,56 @@
+// CHALLENGE NAME-  Grasshopper - Check for factor
+
+// DESCRIPTION:
+
+// This function should test if the factor is a factor of base.
+
+// Return true if it is a factor or false if it is not.
+
+// About factors
+// Factors are numbers you can multiply together to get another number.
+
+// 2 and 3 are factors of 6 because: 2 * 3 = 6
+
+// You can find a factor by dividing numbers. If the remainder is 0 then the number is a factor.
+// You can use the mod operator (%) in most languages to check for a remainder
+// For example 2 is not a factor of 7 because: 7 % 2 = 1
+
+// Note: base is a non-negative number, factor is a positive number.
+
+
+// // ***STARTER CODE***
+
+// function checkForFactor (base, factor) {
+//     // code here
+//   }
+ 
+// //   ******TEST CASES*****
+
+// const Test = require('@codewars/test-compat');
+
+// describe('check for factor', function () {
+//   it('should return true', function () {
+//     Test.assertEquals(checkForFactor(10,2), true)
+//     Test.assertEquals(checkForFactor(63,7), true)
+//     Test.assertEquals(checkForFactor(2450,5), true)
+//     Test.assertEquals(checkForFactor(24612,3), true)
+//   })
+//   it('should return false', function () {
+//     Test.assertEquals(checkForFactor(9,2), false)
+//     Test.assertEquals(checkForFactor(653,7), false)
+//     Test.assertEquals(checkForFactor(2453,5), false)
+//     Test.assertEquals(checkForFactor(24617,3), false)
+//   })
+// })
+ 
+// ******MY ANSWER********
+
+function checkForFactor (base, factor) {
+    // code here
+    if(base%factor===0){
+      return true
+    }
+    else {
+      return false
+    }
+  }
