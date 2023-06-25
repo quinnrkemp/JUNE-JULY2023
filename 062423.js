7df41578bf688f037d3fdf6cc8dfb0a47f4787dd
quinnrkemp <a467814@gmail.com>
2023-06-24 23:18:50 -0400

    coding chall for 062423

---
+++ 062423.js
@@ -0,0 +1,34 @@
+// CHALLENGE NAME-  HELLO WORLD
+
+// DESCRIPTION:
+
+// Make a simple function called greet that returns the most-famous "hello world!".
+
+// Style Points
+// Sure, this is about as easy as it gets. But how clever can you be to create the most creative 
+// "hello world" you can think of? What is a "hello world" solution you would want to show your friends?
+
+
+// // ***STARTER CODE***
+
+// // Write a function "greet" that returns "hello world!" 
+
+// //   ******TEST CASES*****
+
+// const assert = require("chai").assert;
+
+// describe("Testing function", function() {
+//   it("Is it a function?", function() {
+//     assert.strictEqual(typeof greet, "function", "greet should be a function");
+//   });
+//   it("Correct return-value?", function() {
+//     assert.strictEqual(greet(), "hello world!");
+//   });
+// }); 
+
+
+// ******MY ANSWER********
+
+function greet(hello){
+    return "hello world!"
+      }
