3f5b97659078040eab73eb204f29a174415d9440
quinnrkemp <a467814@gmail.com>
2023-07-19 23:55:36 -0400

    coding chall for 071923

---
+++ 071923.js
@@ -0,0 +1,49 @@
+// CHALLENGE NAME-  Grasshopper - Messi Goals
+
+// DESCRIPTION:
+
+// Messi's Goal Total
+// Use variables to find the sum of the goals Messi scored in 3 competitions
+
+// Information
+// Messi goal scoring statistics:
+
+// Competition	Goals
+// La Liga	43
+// Champions League	10
+// Copa del Rey	5
+
+// Task
+// Create these three variables and store the appropriate values using 
+// the table above:
+// laLigaGoals
+// championsLeagueGoals
+// copaDelReyGoals
+// Create a fourth variable named totalGoals that stores the sum of all 
+// of Messi's goals for this year.
+
+// // ***STARTER CODE***
+
+// var laLigaGoals
+// var championsLeagueGoals
+// var copaDelReyGoals
+
+// var totalGoals
+
+// //   ******TEST CASES*****
+
+// const Test = require('@codewars/test-compat');
+
+// describe("total goals", function(){
+//     it ("should equal the total", function () {
+//       Test.assertEquals(totalGoals, 58)
+//     })
+// })
+
+// ******MY ANSWER********
+
+let laLigaGoals= 43
+let championsLeagueGoals= 10
+let copaDelReyGoals= 5
+
+let totalGoals= laLigaGoals + championsLeagueGoals + copaDelReyGoals
