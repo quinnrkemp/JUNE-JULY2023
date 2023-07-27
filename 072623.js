51936b4ee19e8a701e77e7d8b9aa687745d26aba
quinnrkemp <a467814@gmail.com>
2023-07-26 22:19:10 -0400

    coding chall for 072623

---
+++ 072623.js
@@ -0,0 +1,46 @@
+// CHALLENGE NAME-  Who ate the cookie?
+
+// DESCRIPTION:
+
+// For this problem you must create a program that says who ate the last cookie. 
+// If the input is a string then "Zach" ate the cookie. If the input is a float or 
+// an int then "Monica" ate the cookie. If the input is anything else "the dog" ate 
+// the cookie. The way to return the statement is: "Who ate the last cookie? It was 
+// (name)!"
+
+// Ex: Input = "hi" --> Output = "Who ate the last cookie? It was Zach! (The reason 
+// you return Zach is because the input is a string)
+
+// Note: Make sure you return the correct message with correct spaces and punctuation.
+
+// // ***STARTER CODE***
+
+// function cookie(x){
+// }
+
+// //   ******TEST CASES*****
+
+// describe("Tests", () => {
+//     it("test", () => {
+//   Test.assertEquals(cookie("Ryan"), "Who ate the last cookie? It was Zach!")
+//   Test.assertEquals(cookie(26), "Who ate the last cookie? It was Monica!")
+//   Test.assertEquals(cookie(2.3), "Who ate the last cookie? It was Monica!")
+//   Test.assertEquals(cookie(true), "Who ate the last cookie? It was the dog!")
+  
+//     });
+//   });
+
+// ******MY ANSWER********
+
+function cookie(x){
+    if(typeof x==="string"){
+    return `Who ate the last cookie? It was Zach!`
+  }
+  else if(typeof x==="number"){
+    return `Who ate the last cookie? It was Monica!`
+  }
+  else {
+    return `Who ate the last cookie? It was the dog!`
+  }  
+    // ...
+  }
