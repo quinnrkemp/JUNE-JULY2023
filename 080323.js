d91baa137df33f6c62f40ee5b0f7ce0fc1f57895
quinnrkemp <a467814@gmail.com>
2023-08-03 23:38:47 -0400

    coding chall for 080323

---
+++ 080323.js
@@ -0,0 +1,45 @@
+// CHALLENGE NAME-  Ensure question
+
+// DESCRIPTION:
+
+// Given a string, write a function that returns the string with a 
+// question mark ("?") appends to the end, unless the original 
+// string ends with a question mark, in which case, returns the 
+// original string.
+
+// For example (Input --> Output)
+
+// "Yes" --> "Yes?" 
+// "No?" --> "No?"
+
+// // ***STARTER CODE***
+
+// function ensureQuestion(s) {
+//     // Code here
+//   }
+
+// //   ******TEST CASES*****
+
+// const chai = require("chai");
+// const assert = chai.assert;
+// chai.config.truncateThreshold=0;
+
+// describe("Ensure question", () => {
+//   it("Fixed tests", () => {
+//     assert.strictEqual(ensureQuestion(""),"?","Expected: '?'");
+//     assert.strictEqual(ensureQuestion("Yes"),"Yes?","Expected: '?'");
+//     assert.strictEqual(ensureQuestion("No?"),"No?","Expected: '?'");
+//   });
+// }); 
+
+// ******MY ANSWER********
+
+function ensureQuestion(s) {
+    if (s.includes('?')!==true) {
+     return `${s}?`
+     }
+    else {
+       return s 
+     }
+     // Code here
+   }
