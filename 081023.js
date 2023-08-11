d00151084399aae3495afc137ef64354e4ceab78
quinnrkemp <a467814@gmail.com>
2023-08-10 21:25:02 -0400

    coding chall for 081023

---
+++ 081023.js
@@ -0,0 +1,37 @@
+// CHALLENGE NAME-  Do you speak "English"
+
+// DESCRIPTION:
+
+// Given a string of arbitrary length with any ascii characters. Write a 
+// function to determine whether the string contains the whole word "English".
+
+// The order of characters is important -- a string "abcEnglishdef" is correct 
+// but "abcnEglishsef" is not correct.
+
+// Upper or lower case letter does not matter -- "eNglisH" is also correct.
+
+// Return value as boolean values, true for the string to contains "English", 
+// false for it does not.
+
+// // ***STARTER CODE***
+
+// function spEng(sentence){
+//     //write your code here
+//     } 
+
+// //   ******TEST CASES*****
+
+// describe("Tests", () => {
+//     it("test", () => {
+//   Test.assertEquals(spEng("english"), true);
+//   Test.assertEquals(spEng("egnlish"), false);
+//     });
+//   });
+
+// ******MY ANSWER********
+
+function spEng(sentence){
+    return sentence.toLowerCase().includes("english")
+     
+      //write your code here
+    }
