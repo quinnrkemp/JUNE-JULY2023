65aa1e09f0432b45f72d0c8284c07f49982ebdb4
quinnrkemp <a467814@gmail.com>
2023-08-15 23:55:28 -0400

    coding chall for 081523

---
+++ 081523.js
@@ -0,0 +1,46 @@
+// CHALLENGE NAME- Polish alphabet 
+
+// DESCRIPTION:
+
+// There are 32 letters in the Polish alphabet: 9 vowels and 23 consonants.
+
+// Your task is to change the letters with diacritics:
+
+// ą -> a,
+// ć -> c,
+// ę -> e,
+// ł -> l,
+// ń -> n,
+// ó -> o,
+// ś -> s,
+// ź -> z,
+// ż -> z
+// and print out the string without the use of the Polish letters.
+
+// For example:
+
+// "Jędrzej Błądziński"  -->  "Jedrzej Bladzinski"
+
+// // ***STARTER CODE***
+
+// function correctPolishLetters (string) {
+//     // your code
+//   }
+
+// //   ******TEST CASES*****
+
+// describe("Tests", () => {
+//     it("test", () => {
+//   Test.assertEquals(correctPolishLetters("Jędrzej Błądziński"),"Jedrzej Bladzinski");
+//   Test.assertEquals(correctPolishLetters("Lech Wałęsa"),"Lech Walesa");
+//   Test.assertEquals(correctPolishLetters("Maria Skłodowska-Curie"),"Maria Sklodowska-Curie");
+  
+//     });
+//   });
+
+// ******MY ANSWER********
+
+function correctPolishLetters (string) {
+    var dict = {'ą':'a','ć':'c','ę':'e','ł':'l','ń':'n','ó':'o','ś':'s','ź':'z','ż':'z'};
+    return string.replace(/[ąćęłńóśźż]/g, match => dict[match]);
+  }
