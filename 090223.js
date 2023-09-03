99c8054aa7bed10ebfa79ec515d14dff4fd9c9f9
quinnrkemp <a467814@gmail.com>
2023-09-02 22:34:39 -0400

    coding chall for 090223

---
+++ 090223.js
@@ -0,0 +1,110 @@
+// CHALLENGE NAME-  Find the Position
+
+// DESCRIPTION:
+
+// When provided with a letter, return its position in the alphabet.
+
+// Input :: "a"
+
+// Ouput :: "Position of alphabet: 1"
+
+// // ***STARTER CODE***
+
+// function position(letter){
+//     //Write your own Code!
+//     }
+
+// //   ******TEST CASES*****
+
+// describe("Tests", () => {
+//     it("test", () => {
+//   Test.assertEquals(position("a"),"Position of alphabet: 1");
+//   Test.assertEquals(position("z"),"Position of alphabet: 26");
+//   Test.assertEquals(position("e"),"Position of alphabet: 5");
+//     });
+//   }); 
+
+// ******MY ANSWER********
+
+function position(ltt){
+    if (ltt==='a'){
+      return "Position of alphabet: 1"
+    }
+    else if (ltt==='b'){
+      return "Position of alphabet: 2"
+    }
+      else if (ltt==='c'){
+      return "Position of alphabet: 3"
+    }
+      else if (ltt==='d'){
+      return "Position of alphabet: 4"
+    }
+      else if (ltt==='e'){
+      return "Position of alphabet: 5"
+    }
+      else if (ltt==='f'){
+      return "Position of alphabet: 6"
+    }
+      else if (ltt==='g'){
+      return "Position of alphabet: 7"
+    }
+      else if (ltt==='h'){
+      return "Position of alphabet: 8"
+    }
+      else if (ltt==='i'){
+      return "Position of alphabet: 9"
+    }
+      else if (ltt==='j'){
+      return "Position of alphabet: 10"
+    }
+      else if (ltt==='k'){
+      return "Position of alphabet: 11"
+    }
+      else if (ltt==='l'){
+      return "Position of alphabet: 12"
+    }
+      else if (ltt==='m'){
+      return "Position of alphabet: 13"
+    }
+      else if (ltt==='n'){
+      return "Position of alphabet: 14"
+    }
+      else if (ltt==='o'){
+      return "Position of alphabet: 15"
+    }
+      else if (ltt==='p'){
+      return "Position of alphabet: 16"
+    }
+      else if (ltt==='q'){
+      return "Position of alphabet: 17"
+    }
+      else if (ltt==='r'){
+      return "Position of alphabet: 18"
+    }
+      else if (ltt==='s'){
+      return "Position of alphabet: 19"
+    }
+      else if (ltt==='t'){
+      return "Position of alphabet: 20"
+    }
+      else if (ltt==='u'){
+      return "Position of alphabet: 21"
+    }
+      else if (ltt==='v'){
+      return "Position of alphabet: 22"
+    }
+      else if (ltt==='w'){
+      return "Position of alphabet: 23"
+    }
+      else if (ltt==='x'){
+      return "Position of alphabet: 24"
+    }
+      else if (ltt==='y'){
+      return "Position of alphabet: 25"
+    }
+      else {
+      return "Position of alphabet: 26"
+    }
+      
+      //Write your own Code!
+    }
