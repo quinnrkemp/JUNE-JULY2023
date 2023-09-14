ed7b396f1204940959896ed623fa39a270fe6326
quinnrkemp <a467814@gmail.com>
2023-09-13 21:06:58 -0400

    coding chall for 091323

---
+++ 091323.js
@@ -0,0 +1,43 @@
+    // CHALLENGE NAME- Freudian translator (#2)
+
+    // DESCRIPTION:
+
+    // In this kata, the function will take a string 
+    // as its argument, and return a string with every 
+    // word replaced by the explanation to everything, 
+    // according to Freud. (replace every word in a given 
+    // string with the word "sex".) 
+    
+    // Note that an empty string, or no arguments, should 
+    // return an empty string.
+
+    // // ***STARTER CODE***
+
+    // function toFreud(string) {
+    //     return result;
+    //     }
+
+    // //   ******TEST CASES*****
+    
+    // describe("Tests", () => {
+    //     it("test", () => {
+    //   Test.assertSimilar(toFreud(""), "");
+    //   Test.assertSimilar(toFreud("test"), "sex");
+    //   Test.assertSimilar(toFreud("This is a test"), "sex sex sex sex");
+    //   Test.assertSimilar(toFreud("This is a longer test"), "sex sex sex sex sex");
+    //   Test.assertSimilar(toFreud("You're becoming a true freudian expert"), "sex sex sex sex sex sex");
+    //     });
+    //   });
+
+    // ******MY ANSWER********
+
+    function toFreud(string) {
+        if (string==='') {
+          return ""
+        }
+         else { 
+           return string.split(` `).fill(`sex`).join(` `)
+           }
+        //   return result;
+        }
+        
