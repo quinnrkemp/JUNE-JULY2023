5934651b4a811ebe62e91e8ff5fd274d1acca651
quinnrkemp <a467814@gmail.com>
2023-09-28 17:27:03 -0400

    coding chall for 092823

---
+++ 092823.js
@@ -0,0 +1,38 @@
+    // CHALLENGE NAME-  Printing Array elements with Comma delimiters
+
+    // DESCRIPTION:
+
+    // Input: Array of elements
+
+    // ["h","o","l","a"]
+    
+    // Output: String with comma delimited elements of the array in th same order.
+    
+    // "h,o,l,a"
+
+    // // ***STARTER CODE***
+
+    // function printArray(array){
+    //     //show me the code!
+    //   }
+
+    // //   ******TEST CASES*****
+    
+    // const Test = require('@codewars/test-compat');
+
+    // describe("Tests", () => {
+    //   it("test", () => {
+    // var data = [2,4,5,2];
+    // Test.expect( printArray( data ) ==
+    //   "2,4,5,2", "int test failed" )
+    //   });
+    // });
+
+    // ******MY ANSWER********
+
+    function printArray(array){
+        return array
+          
+          // return ar.split(' ').join(',')
+          //show me the code!
+        }
